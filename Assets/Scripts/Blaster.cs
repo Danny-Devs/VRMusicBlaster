01b6b6f9ec6fc1703a2815efa63b07973990c04b
Daniel Ahn <et3rnal.d@gmail.com>
2019-06-26 18:24:48 -0400

    committing unity files

---
+++ Assets/Scripts/Blaster.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR;
+
+public class Blaster : MonoBehaviour
+{
+    [Header("Input")]
+    public SteamVR_Action_Boolean m_FireAction;
+    public SteamVR_Action_Boolean m_ReloadAction;
+
+    [Header("Settings")]
+    public int m_Force = 10;
+    public int m_MaxProjectileCount = 6;
+    public float m_ReloadTime = 1.0f;
+
+    [Header("References")]
+    public Transform m_Barrel;
+    public GameObject m_ProjectilePrefab;
+    public Text m_AmmoOutput;
+
+    private bool m_IsReloading = false;
+    private int m_FiredCount = 0;
+    private SteamVR_Behaviour_Pose m_Pose;
+    private Animator m_Animator;
+    private ProjectilePool m_ProjectilePool;
+
+    private void Awake()
+    {
+        m_Pose = GetComponentInParent<SteamVR_Behaviour_Pose>();
+        m_Animator = GetComponent<Animator>();
+        m_ProjectilePool = new ProjectilePool(m_ProjectilePrefab, m_MaxProjectileCount);
+    }
+
+    private void Start()
+    {
+        UpdateFireCount(0);
+    }
+
+    private void Update()
+    {
+        if (m_IsReloading)
+            return;
+
+        if (m_FireAction.GetStateDown(m_Pose.inputSource))
+        {
+            m_Animator.SetBool("Fire", true);
+            Fire();
+        }
+
+        if (m_FireAction.GetStateUp(m_Pose.inputSource))
+        {
+            m_Animator.SetBool("Fire", false);
+        }
+
+        if (m_ReloadAction.GetStateDown(m_Pose.inputSource))
+        {
+            StartCoroutine(Reload());
+        }
+    }
+
+    private void Fire()
+    {
+        if (m_FiredCount >= m_MaxProjectileCount)
+        {
+            return;
+        }
+
+        Projectile targetProjectile = m_ProjectilePool.m_Projectiles[m_FiredCount];
+        targetProjectile.Launch(this);
+
+        UpdateFireCount(m_FiredCount + 1);
+    }
+
+    private IEnumerator Reload()
+    {
+        if (m_FiredCount == 0)
+            yield break;
+
+        m_AmmoOutput.text = "-";
+        m_IsReloading = true;
+
+        m_ProjectilePool.DisableAllProjectiles();
+
+        yield return new WaitForSeconds(m_ReloadTime);
+
+        UpdateFireCount(0);
+        m_IsReloading = false;
+    }
+
+    private void UpdateFireCount(int newValue)
+    {
+        m_FiredCount = newValue;
+        m_AmmoOutput.text = (m_MaxProjectileCount - m_FiredCount).ToString();
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float m_Lifetime = 5f;
+    private Rigidbody m_Rigidbody;
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        SetInactive();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        SetInactive();
+    }
+
+    public void Launch(Blaster blaster)
+    {
+        
+        transform.position = blaster.m_Barrel.position;
+        transform.rotation = blaster.m_Barrel.rotation;
+
+        gameObject.SetActive(true);
+
+        // Fire and track
+        m_Rigidbody.AddRelativeForce(Vector3.forward * blaster.m_Force, ForceMode.Impulse);
+        StartCoroutine(TrackLifetime());
+    }
+
+    private IEnumerator TrackLifetime()
+    {
+        yield return new WaitForSeconds(m_Lifetime);
+        SetInactive();
+    }
+
+    public void SetInactive()
+    {
+        m_Rigidbody.velocity = Vector3.zero;
+        m_Rigidbody.angularVelocity = Vector3.zero;
+        gameObject.SetActive(false);
+    }
+}
